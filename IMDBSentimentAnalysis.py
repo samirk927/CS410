732c33e0fd2919325fa8b930955f8d7d63a1c21a
samirk927 <samirk927@gmail.com>
2018-12-16 18:39:30 -0600

    Update IMDBSentimentAnalysis.py

--- IMDBSentimentAnalysis.py
+++ IMDBSentimentAnalysis.py
@@ -64,8 +64,8 @@ def imdb_data_preprocess(inpath, outpath="./", name="training_dataset.csv", mix=
     else:
         Dataset = list(zip(indices,text))
     
-    if mix:
-        np.random.shuffle(Dataset)
+    #if mix:
+    np.random.shuffle(Dataset)
     if train_data:
         df = pd.DataFrame(data = Dataset, columns=['row_Number', 'text', 'polarity'])
         df.to_csv(outpath+name, index=False, header=True)
@@ -218,7 +218,7 @@ if __name__ == "__main__":
     print ("Applying the stochastic descent")
     Y_tf_bi = stochastic_descent(Xtrain_tf_bi, Ytrain, Xtrain_tf_bi)
     print ("Stochastic descent applied")
-    print ("Accuracy for the Unigram TFIDF Model is ", accuracy(Ytrain, Y_tf_bi))
+    print ("Accuracy for the BIgram TFIDF Model is ", accuracy(Ytrain, Y_tf_bi))
     print ("\n")
 
 
